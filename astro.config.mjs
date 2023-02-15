722eec91322723247862917a5d4b734d33713b26
Santiago <santiagocortes996@hotmail.com>
2023-02-15 18:35:24 -0300

    integrating tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,64 +2,70 @@ import { defineConfig } from 'astro/config';
 import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    NetlifyCMS({
-      adminPath: '/blog/admin',
-      config: {
-        // Use Netlify’s “Git Gateway” authentication and target our default branch
-        backend: {
-          name: 'git-gateway',
-          branch: 'latest',
-        },
-        // Configure where our media assets are stored & served from
-        media_folder: 'public/assets/blog',
-        public_folder: '/assets/blog',
-        // Configure the content collections
-        collections: [
-          {
-            name: "blog",
-            label: "Blog Posts",
-            label_singular: "Blog Post",
-            folder: "src/pages/posts",
-            create: true,
-            delete: true,
-            slug: "{{year}}-{{month}}-{{day}}-{{slug}}",
-            fields: [
-              { label: "Title", name: "title", widget: "string" },
-              { label: "Description", name: "description", widget: "string" },
-              { label: "Hero Image", name: "heroImage", widget: "image" },
-              {
-                name: "author",
-                widget: "string",
-                label: "Author Name",
-                required: false,
-              },
-              {
-                name: "authorURL",
-                widget: "string",
-                label: "Author URL",
-                required: false,
-              },
-              {
-                name: "publishDate",
-                widget: "datetime",
-                format: "DD MMM YYYY",
-                date_format: "DD MMM YYYY",
-                time_format: false,
-                label: "Publish Date",
-              },
-              { label: "Body", name: "body", widget: "markdown" },
-              {
-                name: "layout",
-                widget: "hidden",
-                default: "../../layouts/BlogPostLayout.astro",
-              },
-            ],
-          },
-        ],
+  integrations: [NetlifyCMS({
+    adminPath: '/blog/admin',
+    config: {
+      // Use Netlify’s “Git Gateway” authentication and target our default branch
+      backend: {
+        name: 'git-gateway',
+        branch: 'latest'
       },
-      previewStyles: ['/src/styles/blog.css'],
-    }),
-  ],
+      // Configure where our media assets are stored & served from
+      media_folder: 'public/assets/blog',
+      public_folder: '/assets/blog',
+      // Configure the content collections
+      collections: [{
+        name: "blog",
+        label: "Blog Posts",
+        label_singular: "Blog Post",
+        folder: "src/pages/posts",
+        create: true,
+        delete: true,
+        slug: "{{year}}-{{month}}-{{day}}-{{slug}}",
+        fields: [{
+          label: "Title",
+          name: "title",
+          widget: "string"
+        }, {
+          label: "Description",
+          name: "description",
+          widget: "string"
+        }, {
+          label: "Hero Image",
+          name: "heroImage",
+          widget: "image"
+        }, {
+          name: "author",
+          widget: "string",
+          label: "Author Name",
+          required: false
+        }, {
+          name: "authorURL",
+          widget: "string",
+          label: "Author URL",
+          required: false
+        }, {
+          name: "publishDate",
+          widget: "datetime",
+          format: "DD MMM YYYY",
+          date_format: "DD MMM YYYY",
+          time_format: false,
+          label: "Publish Date"
+        }, {
+          label: "Body",
+          name: "body",
+          widget: "markdown"
+        }, {
+          name: "layout",
+          widget: "hidden",
+          default: "../../layouts/BlogPostLayout.astro"
+        }]
+      }]
+    },
+    previewStyles: ['/src/styles/blog.css']
+  }), tailwind()]
 });
