2589954ac77d940c79e7d1a51c32d887107989d3
Santiago <santiagocortes996@hotmail.com>
2023-02-15 14:43:26 -0300

    config heroImage render

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ export default defineConfig({
             fields: [
               { label: "Title", name: "title", widget: "string" },
               { label: "Description", name: "description", widget: "string" },
-              { label: "Featured Image", name: "heroImage", widget: "image" },
+              { label: "Hero Image", name: "heroImage", widget: "image" },
               {
                 name: "author",
                 widget: "string",
--- src/pages/posts/2023-02-15-soy-el-titulo.md
+++ src/pages/posts/2023-02-15-soy-el-titulo.md
@@ -1,7 +1,7 @@
 ---
 title: Soy el titulo
 description: Soy la descripcion
-thumbnail: /assets/blog/blog.jpg
+heroImage: /assets/blog/blog.jpg
 author: soy el autor
 authorURL: https://github.com/Santaas96/astro-netlify-cms-starter
 publishDate: 15 Feb 2023
